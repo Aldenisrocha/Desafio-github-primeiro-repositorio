ff4665cc7d038edaea91e9bececaad669ed5f34d
Aldenis Rocha <aldenesmaster@hotmail.com>
2022-05-31 21:14:54 -0300

    inclusão codigo java aula ADS

---
+++ Curso de java/MeuPrimeiroProgramaJava.java
@@ -0,0 +1,23 @@
+// salvar como MeuPrimeiroProgramaJava.java
+
+// nome da classe
+class MeuPrimeiroProgramaJava
+{
+   // modulo pricipal com entrada linha de comando
+   public static void main (String entrada[])
+   {
+      // declaração de variaveis
+            int inteiro = 47;
+            char caracter = 'f';
+            double real = 1.65;
+            String frase = " Aldenis Rocha";
+            boolean VF = true;
+            
+            if (VF == true)
+            {
+               System.out.println("Eu sou o " + frase + " tenho" + inteiro + " anos e tenho " + real + " m de altura");
+            }
+            
+            System.exit(0);
+   }
+}
